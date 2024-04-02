f3fe4098ae38f9a8d7c9910726c392528fc888dc
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 03:44:39 -0500

    Add files via upload

---
+++ DatasizeRange.java
@@ -0,0 +1,35 @@
+
+import java.lang.*;
+
+public class DatasizeRange
+{
+   public static void main(String args[])
+   {
+    
+     System.out.println("Int min is " +Integer.MIN_VALUE);
+     System.out.println("Integer Bytes is " +Integer.BYTES);
+     System.out.println("Int min is "+Integer.MAX_VALUE);
+
+
+     System.out.println("BYTE min is " +Byte.MIN_VALUE);
+     System.out.println("BYTE Bytes is " +Byte.BYTES);
+     System.out.println("BYTE min is "+Byte.MAX_VALUE);
+ 
+     
+     System.out.println("SHORT min is " +Short.MIN_VALUE);
+     System.out.println("SHORT Bytes is " +Short.BYTES);
+     System.out.println("SHORT min is "+Short.MAX_VALUE);
+
+
+     System.out.println("LONG min is " +Long.MIN_VALUE);
+     System.out.println("LONG Bytes is " +Long.BYTES);
+     System.out.println("LONG min is "+Long.MAX_VALUE);
+ 
+    
+     
+     
+
+     
+
+   }
+}
